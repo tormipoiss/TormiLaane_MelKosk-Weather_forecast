2251855b6a70946af092c99a5c6fe6159f4a5dba
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-09-11 08:49:45 +0300

    Merge

--- Weather-forecast/Weather-forecast/Controllers/HomeController.cs
+++ Weather-forecast/Weather-forecast/Controllers/HomeController.cs
@@ -8,12 +8,10 @@ namespace Weather_forecast.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly WeatherAPIHandler _weatherAPIHandler;
 
-        public HomeController(ILogger<HomeController> logger, WeatherAPIHandler weatherAPIHandler)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _weatherAPIHandler = weatherAPIHandler;
         }
 
         public IActionResult Index()
@@ -24,14 +22,6 @@ namespace Weather_forecast.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-        [HttpPost("Home/City")]
-        public async Task<IActionResult> SearchResults([FromForm] string? city)
-        {
-            if (string.IsNullOrEmpty(city)) return BadRequest("City can not be null!");
-            var result = await _weatherAPIHandler.FetchDataAsync(city);
-            if (result == null) return BadRequest($"Failed to fetch weather data for {city}");
-            return View(result);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Weather-forecast/Weather-forecast/Controllers/WeatherController.cs
+++ Weather-forecast/Weather-forecast/Controllers/WeatherController.cs
@@ -2,43 +2,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Weather_forecast.Data;
 using Weather_forecast.Models;
+using Weather_forecast.Services;
 
 namespace Weather_forecast.Controllers
 {
     public class WeatherController : Controller
     {
         private readonly DatabaseContext _context;
+        private readonly WeatherAPIHandler _weatherAPIHandler;
 
-        public WeatherController(DatabaseContext context)
+        public WeatherController(DatabaseContext context, WeatherAPIHandler weatherAPIHandler)
         {
             _context = context;
+            _weatherAPIHandler = weatherAPIHandler;
         }
+        //[HttpGet]
+        //public IActionResult SearchWeatherByCity(History model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View("~/Views/Home/Index.cshtml", model);
+        //    }
+        //    if (model.City == null || model.City == "")
+        //    {
+        //        return View("~/Views/Home/Index.cshtml");
+        //    }
+        //    SaveCityToHistory(model.City);
+        //    return View("~/Views/Home/Index.cshtml");
+        //}
+
         [HttpGet]
-        public IActionResult SearchWeatherByCity(History model)
+        public async Task<IActionResult> SearchWeatherByCity(History model)
         {
-            if (!ModelState.IsValid)
-            {
-                return View("~/Views/Home/Index.cshtml", model);
-            }
             if (model.City == null || model.City == "")
             {
                 return View("~/Views/Home/Index.cshtml");
             }
+            var result = await _weatherAPIHandler.FetchDataAsync(model.City);
+            if (result == null) return BadRequest($"Failed to fetch weather data for {model.City}");
             SaveCityToHistory(model.City);
-            return View("~/Views/Home/Index.cshtml");
+            return View(result);
         }
 
         [HttpPost]
         public IActionResult SaveCityToHistory(string city)
         {
             var history = new History();
-            //ulong newId = 0;
-            //if (_context.SearchHistory.Any())
-            //{
-            //    newId = _context.SearchHistory.Max(x => x.Id) + 1;
-            //}
-            //else { newId = 1; }
-            //history.Id = newId;
             history.City = city;
             history.DateAndTimeOfSearch = DateTime.Now;
             _context.SearchHistory.Add(history);
